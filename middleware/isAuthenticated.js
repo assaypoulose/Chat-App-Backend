7fdd16d35a1c09d4f739c6411d888b372499966b
Assay Poulose <94008297+assaypoulose@users.noreply.github.com>
2024-06-13 13:11:38 +0200

    made changes to isAuthenticated file

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,23 +1,23 @@
 import jwt from "jsonwebtoken";
+
 const isAuthenticated = async (req, res, next) => {
     try {
         const token = req.cookies.token;
         if (!token) {
-            return res.status(401).json({ message: "User not authenticated." })
-        };
-        const decode = await jwt.verify(token, process.env.JWT_SECRET_KEY);
-        if (!decode) {
+            return res.status(401).json({ message: "User not authenticated." });
+        }
+
+        const decoded = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+        if (!decoded) {
             return res.status(401).json({ message: "Invalid token" });
-        };
-        req.id = decode.userId;
+        }
+
+        req.id = decoded.userId; // Set the userId from decoded token to req.id
         next();
     } catch (error) {
-        console.log(error);
+        console.error("Error in authentication middleware:", error);
+        return res.status(500).json({ message: "Internal Server Error" });
     }
 };
-export default isAuthenticated;
 
-const req = {
-    id: "",
-}
-req.id = "sdlbgnjdfn"
+export default isAuthenticated;
