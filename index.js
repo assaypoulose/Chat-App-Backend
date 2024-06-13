1c18b40b04876f3b4fb5bda0b004f8c5e23c1463
Assay Poulose <94008297+assaypoulose@users.noreply.github.com>
2024-06-13 13:48:53 +0200

    made changes to isAuthenticated file again

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// const express = require('express')// method-1
-import express from 'express'; // method-2
+import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/database.js';
 import userRoute from './routes/userRoute.js';
@@ -14,21 +13,17 @@ connectDB();
 
 const PORT = process.env.PORT || 5001;
 
-
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser());
-
-const corsOptions = {
+app.use(cookieParser()); // Make sure this is before your routes
+const corsOption = {
     origin: 'http://localhost:3000',
-    credentials: true, // Allow credentials (cookies)
+    credentials: true
 };
+app.use(cors(corsOption));
 
-app.use(cors(corsOptions));
-
-//routes
-// http://localhost:8080/api/v1/user/register
+// routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/message", messageRoute);
 
